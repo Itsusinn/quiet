be67265b9fb13e54eecb88462e13a1e5cbb3a6fc
Itsusinn奕䜣 <itsusinn@foxmail.com>
2021-02-04 21:03:49 +0800

    feat(Scene): Add drawable and scene logic

--- src/main/kotlin/io/github/itsusinn/extension/org/lwjgl/render/Scene.kt
+++ src/main/kotlin/io/github/itsusinn/extension/org/lwjgl/render/Scene.kt
@@ -2,14 +2,61 @@ package io.github.itsusinn.extension.org.lwjgl.render
 
 import io.github.itsusinn.extension.org.lwjgl.camera.Camera
 import io.github.itsusinn.extension.org.lwjgl.components.GameObject
+import io.github.itsusinn.extension.org.lwjgl.components.SpriteComponent
 import java.util.ArrayList
 
-abstract class Scene(
-    val camera: Camera,
-) {
+interface IScene {
+    val camera: Camera
+    val gameObjects: MutableList<GameObject>
+    val renderer: Renderer
+
+    fun init()
+
+    fun start()
+
+    fun update(dt: Float)
+}
+
+abstract class AbstractScene(
+    override val camera: Camera = Camera(),
+    override val gameObjects: MutableList<GameObject> = ArrayList(),
+    override val renderer: Renderer = Renderer(camera)
+) : IScene {
+
     // protected val renderer = Renderer()
     private val isRunning = false
-    protected var gameObjects: List<GameObject> = ArrayList<GameObject>()
-    abstract fun init()
-    abstract fun update(dt: Float)
+
+    final fun addGameObject(gameObject: GameObject) {
+        gameObjects.add(gameObject)
+        gameObject.getComponent<SpriteComponent>()?.let {
+            renderer.addSpriteRender(it.spriteRender)
+        }
+    }
+
+    final override fun init() {
+        for (gameObject in gameObjects) {
+            gameObject.init()
+        }
+        onInit()
+        renderer.init()
+    }
+    abstract fun onInit()
+
+    final override fun start() {
+        for (gameObject in gameObjects) {
+            gameObject.start()
+        }
+        onStart()
+    }
+    abstract fun onStart()
+
+    final override fun update(dt: Float) {
+        for (gameObject in gameObjects) {
+            gameObject.update(dt)
+        }
+        renderer.draw()
+        onUpdate(dt)
+    }
+
+    abstract fun onUpdate(dt: Float)
 }
--- src/main/kotlin/io/github/itsusinn/quiet/BaseScene.kt
+++ src/main/kotlin/io/github/itsusinn/quiet/BaseScene.kt
@@ -1,12 +1,87 @@
 package io.github.itsusinn.quiet
 
-import io.github.itsusinn.extension.org.lwjgl.camera.Camera
-import io.github.itsusinn.extension.org.lwjgl.render.Scene
+import io.github.itsusinn.extension.org.lwjgl.components.GameObject
+import io.github.itsusinn.extension.org.lwjgl.components.ModelTransformation
+import io.github.itsusinn.extension.org.lwjgl.components.SpriteComponent
+import io.github.itsusinn.extension.org.lwjgl.render.AbstractScene
+import io.github.itsusinn.extension.org.lwjgl.render.Sprite
+import io.github.itsusinn.extension.org.lwjgl.render.SpriteSheet
+import io.github.itsusinn.extension.org.lwjgl.texture.Texture
+import io.github.itsusinn.extension.org.lwjgl.unit.Coordinate
+import io.github.itsusinn.extension.org.lwjgl.unit.TexCoordinate
+import io.github.itsusinn.quiet.listener.KeyboardListener
+import kotlinx.coroutines.runBlocking
+import org.joml.Vector4f
+import org.lwjgl.glfw.GLFW.* // ktlint-disable no-wildcard-imports
 
-class BaseScene : Scene(Camera()) {
-    override fun init() {
+class BaseScene : AbstractScene() {
+    init {
+        camera.eye.set(50f, 50f, 20f)
+    }
+    override fun onInit() {
+        runBlocking {
+            KeyboardListener.registerHandler(predicate = { true }) { keys ->
+                if (keys[GLFW_KEY_W]) {
+                    camera.eye.add(0f, 0f, -2f)
+                } else if (keys[GLFW_KEY_S]) {
+                    camera.eye.add(0f, 0f, 2f)
+                }
+                if (keys[GLFW_KEY_A]) {
+                    camera.eye.add(-2f, 0f, 0f)
+                } else if (keys[GLFW_KEY_D]) {
+                    camera.eye.add(2f, 0f, 0f)
+                }
+                if (keys[GLFW_KEY_UP]) {
+                    camera.eye.add(0f, 2f, 0f)
+                } else if (keys[GLFW_KEY_DOWN]) {
+                    camera.eye.add(0f, -2f, 0f)
+                }
+            }
+        }
+
+        val spriteSheet: SpriteSheet = SpriteSheet
+            .cacheCreate(
+                Texture.cacheCreate("assets/images/sprite-sheet.png"),
+                14,
+                2,
+                26
+            )
+        val background = GameObject(
+            "Background",
+            ModelTransformation(),
+            shape = Coordinate(
+                Vector4f(80f, 60f, 0f, 1f),
+                Vector4f(80f, 0f, 0f, 1f),
+                Vector4f(0f, 0f, 0f, 1f),
+                Vector4f(0f, 60f, 0f, 1f),
+            )
+        ).components {
+            + SpriteComponent(
+                sprite = Sprite(
+                    Texture.cacheCreate("assets/images/blank.png"),
+                    TexCoordinate(1920f, 0f, 1080f, 0f)
+                )
+            )
+        }
+        addGameObject(background)
+        val player = GameObject(
+            "player-1",
+            ModelTransformation(),
+            shape = Coordinate(
+                Vector4f(50f, 50f, 1f, 1f),
+                Vector4f(50f, 0f, 1f, 1f),
+                Vector4f(0f, 0f, 1f, 1f),
+                Vector4f(0f, 50f, 1f, 1f),
+            )
+        ).components {
+            + SpriteComponent(sprite = spriteSheet.cut(5))
+        }
+        addGameObject(player)
     }
 
-    override fun update(dt: Float) {
+    override fun onStart() {
+    }
+
+    override fun onUpdate(dt: Float) {
     }
 }
